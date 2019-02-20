8a0f5d6495b84a70c037e6fb81b91944d2578ebc
maniart <mani.art@gmail.com>
2019-02-20 14:32:18 -0500

    WIP

--- client/src/containers/app/index.js
+++ client/src/containers/app/index.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import Admin from '../admin';
 import Canvas from '../prompts/canvas';
 
 const App = () => (
   <div>
     <header>
-      <NavLink to="/admin">Admin</NavLink>
-      <NavLink to="/canvas">Canvas</NavLink>
+      <NavLink 
+        to="/admin"
+        activeClassName="nav-active">
+        Admin
+      </NavLink>
+      <NavLink
+        to="/canvas"
+        activeClassName="nav-active">
+        Canvas
+      </NavLink>
     </header>
 
     <main>
-      <Route exact path="/admin" component={Admin}></Route> 
-      <Route exact path="/canvas" component={Canvas}></Route> 
+      <Switch>
+        <Route 
+          exact
+          activeClassName="nav-active"
+          path="/admin"
+          component={Admin}>
+        </Route> 
+        <Route 
+          exact
+          path="/canvas" 
+          component={Canvas}>
+        </Route>
+      </Switch>
     </main>
   </div>
 );
