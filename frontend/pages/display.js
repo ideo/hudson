47feb4d35523e9645813a4587ded9a12db900922
maniart <mani.art@gmail.com>
2019-03-13 03:26:55 -0400

    WIP gradients

--- frontend/pages/display.js
+++ frontend/pages/display.js
@@ -19,7 +19,7 @@ class Display extends Component {
   };
 
   static async getInitialProps({ query }) {
-    let response = {};
+    let response = null;
     let notFound = false;
 
     try {
@@ -29,6 +29,11 @@ class Display extends Component {
       notFound = true;
     }
     
+    if (!response || !response.hasOwnProperty('id')) {
+      // we either don't have a query.id or the response was not valid 
+      notFound = true;
+    }
+
     return {
       data: response,
       notFound
@@ -41,6 +46,7 @@ class Display extends Component {
   }
 
   componentWillMount() {
+    if (this.props.notFound) return;
     this.socket = openSocket('http://localhost:1337');
   }
 
@@ -52,6 +58,8 @@ class Display extends Component {
   }
   
   componentDidMount() {
+    if (this.props.notFound) return;
+
     this.subscribeToTimer((err, transcription) => {
       if(err) return;
       this.setState({
@@ -59,8 +67,8 @@ class Display extends Component {
       });
     });
 
-    let radius = 8;
-    TweenMax.staggerFromTo('.blob', 4 , {
+    let radius = 20;
+    TweenMax.staggerFromTo('.blob-1', 10 , {
       cycle: {
         attr(i) {
           let r = i*90;
@@ -75,6 +83,50 @@ class Display extends Component {
           let r = i*90+360;
           return {
             transform:`rotate(${r}) translate(${radius},0.1) rotate(-${r})`
+          }      
+        }
+      },
+      ease: Linear.easeIn,
+      repeat: -1
+    });
+
+    TweenMax.staggerFromTo('.blob-2', 58 , {
+      cycle: {
+        attr(i) {
+          let r = i*90;
+          return {
+            transform:`rotate(${-r}) translate(${radius * 2},0.1) rotate(-${r})`
+          }      
+        }
+      }  
+    },{
+      cycle: {
+        attr(i) {
+          let r = i*90+ 360;
+          return {
+            transform:`rotate(${r}) translate(${radius},0.1) rotate(-${r})`
+          }      
+        }
+      },
+      ease: Linear.easeOut,
+      repeat: -1
+    });
+
+    TweenMax.staggerFromTo('.blob-3', 4 , {
+      cycle: {
+        attr(i) {
+          let r = i*90;
+          return {
+            transform:`rotate(${r}) translate(${radius},0.1) rotate(-${r})`
+          }      
+        }
+      }  
+    },{
+      cycle: {
+        attr(i) {
+          let r = i*90+360;
+          return {
+            transform:`rotate(${r}) translate(${radius * 0.1},0.1) rotate(-${r})`
           }      
         }
       },
@@ -95,7 +147,7 @@ class Display extends Component {
     
     return (
       <Layout>
-        {notFound && <h1>Ah nuts. Couln't find the related prompt. Check the URL?</h1>}
+        {notFound && <p className="message-body">Ah nuts. Couln't find the related prompt. Check the URL?</p>}
         
         {!notFound && <p className="message-body">{ transcription }</p>}
         <div className="blobs-container">
@@ -103,24 +155,82 @@ class Display extends Component {
             <defs>
               <filter id="goo">
                 <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10" />
-                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
-                <feBlend in2="goo" in="SourceGraphic" result="mix" />
+                <feColorMatrix in="blur" mode="matrix" values="1 21 21 71  40 61 30 20 0  0 0 1 0 0  0 0 0 18 -7" result="mix" />
               </filter>
-              <linearGradient id="MyGradient1">
-                  <stop offset="50%"  stopColor="rgb(196, 129, 96)"/>
-                  <stop offset="100%" stopColor="rgb(181, 76, 58)"/>
+              
+              <linearGradient id="grad-1" x1="0" y1="0" x2="1" y2="1">
+                <stop offset="0%"  stopColor="rgb(45, 56, 70)" stopOpacity="1"/>
+                <stop offset="70%" stopColor="rgb(210, 156, 107)" stopOpacity="0"/>
+              </linearGradient>
+              <linearGradient id="grad-2" x1="1" y1="0" x2="0" y2="1">
+                <stop offset="0%"  stopColor="rgb(196, 73, 56)" stopOpacity="0.3"/>
+                <stop offset="100%" stopColor="rgb(210, 156, 107)" stopOpacity="1"/>
+              </linearGradient>
+              <linearGradient id="grad-3" x1="0" y1="0" x2="1" y2="0.2">
+                <stop offset="0%"  stopColor="rgb(205, 162, 118)" stopOpacity="0"/>
+                <stop offset="10%" stopColor="rgb(31, 42, 69)" stopOpacity="0.22"/>
+                <stop offset="35%" stopColor="rgb(198, 107, 85)" stopOpacity="0.56"/>
+                <stop offset="55%" stopColor="rgb(88, 79, 71)" stopOpacity="0.36"/>
+                <stop offset="85%" stopColor="rgb(198, 75, 58)" stopOpacity="0.16"/>
               </linearGradient>
             </defs>
-            <mask id="maska">
+            <mask id="mask-1">
               <g className="blobs">
-                <circle className="blob" cx="440" cy="250" r="30" transform="rotate(0) translate(0, 0) rotate(0)"/>
-                <circle className="blob" cx="500" cy="320" r="70"  transform="rotate(0) translate(0, 0) rotate(0)"/>
-                <circle className="blob" cx="300" cy="390" r="40"  transform="rotate(0) translate(0, 0) rotate(0)"/>
-                <circle className="blob" cx="380" cy="390" r="80"  transform="rotate(0) translate(0, 0) rotate(0)"/>
-                <circle className="blob" cx="470" cy="450" r="20"  transform="rotate(0) translate(0, 0) rotate(0)"/>
+                <ellipse className="blob blob-1" cx="40%" cy="50%" rx="40%" ry="80%" transform="rotate(0) translate(0, 0) rotate(0)" />
+                {/*<circle className="blob" cx="500" cy="320" r="70" transform="rotate(0) translate(0, 0) rotate(0)"/>
+                <circle className="blob" cx="300" cy="390" r="40" transform="rotate(0) translate(0, 0) rotate(0)"/>
+                <circle className="blob" cx="380" cy="390" r="80" transform="rotate(0) translate(0, 0) rotate(0)"/>
+                <circle className="blob" cx="470" cy="450" r="20" transform="rotate(0) translate(0, 0) rotate(0)"/>*/}
               </g>
             </mask>
-            <rect x="200" y="200"  mask="url(#maska)" fill="url(#MyGradient1)" width="400" height="400"></rect>
+            <mask id="mask-2">
+              <g className="blobs">
+                {/*<ellipse className="blob" cx="0%" cy="20%" rx="40%" ry="80%" transform="rotate(0) translate(0, 0) rotate(0)" />*/}
+                <ellipse className="blob blob-2" cx="10%" cy="0%" rx="60%" ry="60%" transform="rotate(0) translate(0, 0) rotate(0)" />
+                <ellipse className="blob blob-2" cx="50%" cy="30%" rx="48%" ry="48%" transform="rotate(0) translate(0, 0) rotate(0)" />
+                <ellipse className="blob blob-2" cx="30%" cy="80%" rx="38%" ry="38%" transform="rotate(0) translate(0, 0) rotate(0)" />
+              </g>
+            </mask>
+            <mask id="mask-3">
+              <g className="blobs">
+                <circle className="blob blob-3" cx="73%" cy="30%" r="50%" transform="rotate(0) translate(0, 0) rotate(0)"/>
+                <circle className="blob blob-3" cx="33%" cy="30%" r="20%" transform="rotate(0) translate(0, 0) rotate(0)"/>                
+
+                {/*<circle className="blob" cx="20%" cy="88%" r="40%" transform="rotate(0) translate(0, 0) rotate(0)"/>
+                <circle className="blob" cx="43%" cy="80%" r="80%" transform="rotate(0) translate(0, 0) rotate(0)"/>
+                <circle className="blob" cx="60%" cy="450" r="20%" transform="rotate(0) translate(0, 0) rotate(0)"/>*/}
+              </g>
+            </mask>
+            
+            <rect 
+              style={{ stroke: '#000', strokeWidth: 0 }} 
+              mask="url(#mask-1)"
+              x="0%" 
+              y="0"
+              fill="url(#grad-1)"
+              width="100%"
+              height="100%">
+            </rect>
+            <rect 
+              style={{ stroke: '#000', strokeWidth: 0 }} 
+              mask="url(#mask-2)"
+              x="0%" 
+              y="0"
+              fill="url(#grad-2)"
+              width="100%"
+              height="100%">
+            </rect>
+            <rect 
+              style={{ stroke: '#000', strokeWidth: 0 }} 
+              mask="url(#mask-3)"
+              x="0%" 
+              y="0"
+              fill="url(#grad-3)"
+              width="100%"
+              height="100%">
+            </rect>
+
+            
           </svg>	
         </div>
         
