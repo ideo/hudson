d2ed9ee53c4ddad98f60184b17ea4fe1e121827f
maniart <mani.art@gmail.com>
2019-04-23 16:52:03 -0400

    Update ecosystem file to pass through node.js env vars

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -9,6 +9,11 @@ module.exports = {
             "NODE_ENV": "development"
         },
         env_production: {
+            "DATABASE_HOST": process.env.DATABASE_HOST,
+            "DATABASE_PORT": process.env.DATABASE_PORT,
+            "DATABASE_NAME": process.env.DATABASE_NAME,
+            "DATABASE_USERNAME": process.env.DATABASE_USERNAME,
+            "DATABASE_PASSWORD": process.env.DATABASE_PASSWORD,
             "PORT": 1337,
             "NODE_ENV": "production",
         }
