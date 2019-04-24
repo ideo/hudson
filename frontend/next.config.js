2c285b1867def3f472dc504dba82bded30f9cc62
maniart <mani.art@gmail.com>
2019-04-23 23:56:06 -0400

    Use amazon public dns as base url for frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,7 +7,7 @@ module.exports = withOffline(
   withSass({
     /* any other next.js config goes here */
     publicRuntimeConfig: {
-      BASE_API_URL: isProd ? 'http://172.31.75.13:1337' : 'http://localhost:1337' 
+      BASE_API_URL: isProd ? 'http://ec2-34-225-188-146.compute-1.amazonaws.com:1337' : 'http://localhost:1337' 
     },
     postcssLoaderOptions: { 
       parser: true, 
